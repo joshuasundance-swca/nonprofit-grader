b6be301d2f50ef3e855eea9b75f15afdd9f95e72
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-11-04 19:25:24 -0700

    blah

---
+++ nonprofit-grader/backend/backend/app.py
@@ -0,0 +1,21 @@
+import os
+from fastapi import FastAPI
+from langchain.chat_models import ChatAnthropic, ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langserve import add_routes
+from ocr import qa
+
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple api server using Langchain's Runnable interfaces",
+)
+
+
+prompt = ChatPromptTemplate.from_template("Find organization(s) that deals with {interest}, and tell me about them.")
+
+add_routes(
+    app,
+    qa,
+    path="/nonprofit",
+)
