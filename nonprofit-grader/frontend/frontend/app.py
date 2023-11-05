342dafdc102565f85126dd06ef0b779a90faca60
Leo Walker <leowalker89@gmail.com>
2023-11-04 19:26:14 -0700

    app.py: updated the conclusion page and the application page

--- nonprofit-grader/frontend/frontend/app.py
+++ nonprofit-grader/frontend/frontend/app.py
@@ -28,18 +28,12 @@ def brief_page():
     - Leo Walker
     - Ambro Quach
     """)
-    st.subheader("Mission Statement:")
-    st.subheader("Solution:")
-    st.subheader("Synopsis:")
-    st.markdown("""
-        - Ideation:
-        - Design:
-        - Implementation:
-        - AI Usage:
-        - Constraints:
-        - Considerations:
-        """)
-    st.subheader("Causes We Care:")
+    st.subheader("Problem Statement: ")
+    st.markdown("In the midst of the largest wealth transfer in history, an estimated \$72.6 trillion passing to younger generations, donors are swamped by the choice of 1.3 million charities. Discerning which ones truly align with their values for impactful philanthropy is as formidable as finding a needle in a haystack, despite the $500 billion given annually.")
+    st.subheader("Solution: ")
+    st.markdown("Impact Aligner is an AI-powered platform that extracts and interprets complex data from charity filings. This innovative approach transforms cumbersome and expensive research into a seamless experience, empowering donors to effortlessly discover and contribute to organizations that genuinely contribute to impact goals they care most about. ")
+
+    
 
 # Application Page Content
 def fetch_charities(interests):
@@ -59,18 +53,32 @@ def application_page():
     st.title("What causes are you most passionate about?")
 
     # Text input
-    text_input = st.text_input("Enter your interests")
+    text_input = st.text_input("Enter your interests", key="input")
 
-    # Send button
-    send_button = st.button('Send')
+    # Inline buttons
+    button_container = st.container()
+    button_container.markdown(
+        """
+        <style>
+        .button-container {
+            display: flex;
+            justify-content: space-between;
+            flex-direction: row;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    with button_container:
+        with st.container():
+            # Send button
+            if st.button('Send'):
+                # Fetch and store the charity info in session state
+                st.session_state['charities_info'] = fetch_charities(text_input)
 
     # Initialize session state for storing charities info
     if 'charities_info' not in st.session_state:
         st.session_state['charities_info'] = []
-
-    if send_button and text_input:
-        # Fetch and store the charity info in session state
-        st.session_state['charities_info'] = fetch_charities(text_input)
 
     # Check if there is any charity info in the session state to display
     if st.session_state['charities_info']:
@@ -84,9 +92,8 @@ def application_page():
 
 # Conclusion Page Content
 def conclusion_page():
-    st.title("Conclusion")
-    st.subheader("Goal:")
-    st.subheader("Stretch:")
+    st.subheader("Potential: ")
+    st.markdown("In future iterations, we can enhance our AI platform to track and analyze charity filings over time, spotlighting performance and spotting irregularities. Our expanded metrics will include financial details like compensation and grants, offering donors a transparent view of a charity's genuine impact. This next step isn't just an upgrade—it's a leap towards a transparent, accountable philanthropy sector, aligning with the values of a new generation of donors.")
 
 # Sidebar navigation with session state to remember the last active page
 def main():
